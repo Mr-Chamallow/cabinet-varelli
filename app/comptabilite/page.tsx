b9c2b0adc18bdbe86d1775e64006e07369e47c01
Mr-Chamallow <mrchamallow3@gmail.com>
2026-06-05 19:32:12 +0200

    Base Varelli stable

---
+++ app/comptabilite/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{padding:40}}>
+      <h1>?? Comptabilité</h1>
+      <p>Module en préparation...</p>
+    </main>
+  );
+}
---
+++ app/dossiers/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{padding:40}}>
+      <h1>?? Dossiers</h1>
+      <p>Module en préparation...</p>
+    </main>
+  );
+}
