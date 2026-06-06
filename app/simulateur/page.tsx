e1e0f732a9421a800e5b447d14281d4896675826
Mr-Chamallow <mrchamallow3@gmail.com>
2026-06-06 09:50:13 +0200

    Fix simulateur page

---
+++ app/simulateur/page.tsx
@@ -0,0 +1,10 @@
+﻿"use client";
+
+export default function SimulateurPage() {
+  return (
+    <main style={{padding:40}}>
+      <h1>⚖ Simulateur Cabinet Varelli</h1>
+      <p>Module de facturation RP</p>
+    </main>
+  );
+}
