c14a95cd0a67bd2b057819e7183e5a20972d8143
Mr-Chamallow <mrchamallow3@gmail.com>
2026-07-20 13:30:57 +0200

    Fix: Ajout du SessionProvider dans layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Providers } from "@/components/Providers";
 
 export const metadata: Metadata = {
   title: "Cabinet BullHead",
@@ -14,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body>
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
